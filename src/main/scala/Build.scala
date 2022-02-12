367094ea65d6c06ab0e062c916a0345c40b1daab
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-02-12 10:26:00 +0800

    define method main in Build

--- src/main/scala/Build.scala
+++ src/main/scala/Build.scala
@@ -3,9 +3,11 @@ import chisel3._
 import chisel3.stage._
 import funcunit._
 
-object Build extends App {
-  //  println(getVerilogString(new InstFetch))
-  println("Generating Verilog Code")
-  (new ChiselStage).emitVerilog(new Decode, Array("--target-dir", "build"))
-  println("Done")
+object Build {
+  def main(args: Array[String]): Unit = {
+    //  println(getVerilogString(new InstFetch))
+    println("Generating Verilog Code")
+    (new ChiselStage).emitVerilog(new Decode, Array("--target-dir", "build"))
+    println("Done")
+  }
 }
